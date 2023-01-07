d1e8136ee2e54537a2715339a7fa77f3d6efafdf
charly2045 <charly2045@icloud.com>
2023-01-07 09:34:19 +0100

    Cambiar ruta de cartas

--- src/blackjack/usecases/crear-carta-html.js
+++ src/blackjack/usecases/crear-carta-html.js
@@ -11,7 +11,7 @@ export const crearCartaHtml = (carta) => {
 
     // <img class="carta" src="assets/cartas/2C.png">
     const imgCarta = document.createElement('img');
-    imgCarta.src = `assets/cartas/${ carta }.png`; //3H, JD
+    imgCarta.src = `public/assets/cartas/${ carta }.png`; //3H, JD
     imgCarta.classList.add('carta');
 
     return imgCarta;
